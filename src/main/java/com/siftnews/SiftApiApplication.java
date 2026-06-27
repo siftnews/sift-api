1c406d05a5a27b9787423ceedf37c2a9b08c03b1
Seungwon-Choi <bestwondev@gmail.com>
2026-06-27 16:32:15 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/siftnews/SiftApiApplication.java
@@ -0,0 +1,13 @@
+package com.siftnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiftApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiftApiApplication.class, args);
+    }
+
+}
