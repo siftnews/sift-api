fcc4cdc675538f1c793ee6c43279c7e48fc49c9b
Seungwon-Choi <bestwondev@gmail.com>
2026-08-11 16:10:56 +0900

    test: 통합 테스트 트랜잭션 경계 보정

--- src/test/java/com/siftnews/subscriber/adapter/in/bootstrap/SubscriptionSeedPersistenceIntegrationTest.java
+++ src/test/java/com/siftnews/subscriber/adapter/in/bootstrap/SubscriptionSeedPersistenceIntegrationTest.java
@@ -8,9 +8,11 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 class SubscriptionSeedPersistenceIntegrationTest extends AbstractIntegrationTest {
 
     private static final long TOPIC_ID = 990_001L;
